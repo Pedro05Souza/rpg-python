06d35fff87b6d8f57293855ead248a418a7c4ddc
Pedro <pedroh.fsouzars@gmail.com>
2024-11-15 15:03:13 -0300

    fix: item name being a tuple

--- item.py
+++ item.py
@@ -1,8 +1,11 @@
 class Item():
 
     def __init__(self, name, damage_boost=0, defense_boost=0, health_boost=0):
-        self.name = name,
+        self.name = name
         self.damage_boost = damage_boost
         self.defense_boost = defense_boost
         self.health_boost = health_boost
         self.is_equiped = False
+
+    def __str__(self) -> str:
+        return f"Item: {self.name}, Dano: {self.damage_boost}, Defesa: {self.defense_boost}, Vida: {self.health_boost}"
