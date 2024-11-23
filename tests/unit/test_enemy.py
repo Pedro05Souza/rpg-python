53e413e8bb812e1352fad67c7338f106a22e9700
Pedro <pedroh.fsouzars@gmail.com>
2024-11-23 18:56:08 -0300

    refact: remove unecessaryy lines of code.

--- tests/unit/test_enemy.py
+++ tests/unit/test_enemy.py
@@ -21,15 +21,15 @@ def test_on_enemy_death(character: Character, enemy: Enemy) -> None:
     enemy.on_enemy_death(character)
     assert character.xp == enemy.xp
 
-def test_enemy_drop_low_chance(enemy: Enemy) -> None:
-    enemy.drop_items = [Item(name="Sword", damage_boost=10, defense_boost=5, health_boost=20)]
+def test_enemy_drop_low_chance(enemy: Enemy, item: Item) -> None:
+    enemy.drop_items = [item]
 
     with patch("entities.enemy.random") as mock_random:
         mock_random.return_value = 0.2
         assert enemy._Enemy__enemy_drop() is None
 
-def test_enemy_drop_high_chance(enemy: Enemy) -> None:
-    enemy.drop_items = [Item(name="Sword", damage_boost=10, defense_boost=5, health_boost=20)]
+def test_enemy_drop_high_chance(enemy: Enemy, item: Item) -> None:
+    enemy.drop_items = [item]
 
     with patch("entities.enemy.random") as mock_random:
         mock_random.return_value = 0.8
